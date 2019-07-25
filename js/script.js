46d4e223b9dcc5ddbb0600e2e3a59fbdd3f58cbf
isimbiJessica <jessicaritha@gmail.com>
2019-07-25 17:01:12 +0200

    adding jquerry codes

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ $(document).ready(function(){
     });
 });
 $(document).ready(function(){
-    $(".all").click(function(){
-        $("#p1").toggle();
-        $("#img1").toggle();
+    $(".all1").click(function(){
+        $("#p2").toggle();
+        $("#img2").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".all2").click(function(){
+        $("#p3").toggle();
+        $("#img3").toggle();
     });
 });
